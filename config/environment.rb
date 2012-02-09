7da7385c80f216d1caedad93fec2548e68c4b71f
PhillipKregg <phillipkregg@gmail.com>
2012-02-09 13:58:26 -0600

    Added backbone views, and models along with Kendo UI grid.  The router has a 'user' and 'post' method, and when the link changes, the Kendo datasource is activated and populates the data accordingly.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#This line is needed for the Rails app to be able to use the JSON data
+# that is being returned from the server
 ActiveRecord::Base.include_root_in_json = false
 
 # Initialize the rails application
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,34 +2,184 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function(){
-    
-    var sharableDataSource = new kendo.data.DataSource({
+	
+	
+	
+	
+		
+		window.Navigation = Backbone.View.extend({
+		el: $("#nav"),
+		
+		//navTemplate: _.template($("#nav-template").html()),
+		
+		initialize: function() {			
+			_.bindAll(this, "render");
+			this.nav = $("#nav-template").html();
+			this.render().el;
+		},	
+		
+		render: function() {
+			$(this.el).html(this.nav).fadeIn();
+			return this;
+		}
+		
+		
+		
+		});
+	
+		window.nav = new Navigation;
+		
+		
+		window.UsersView = Backbone.View.extend({
+								
+			initialize: function() {	
+				alert("sup")			
+				_.bindAll(this, "render");
+				this.usersGrid = _.template($("#users-grid-template").html());
+				this.render().el;					
+						
+			},
+			
+			render: function() {
+				
+				$(this.el).html(this.usersGrid).fadeIn();
+				return this;
+			}
+			
+		})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	var PostsData = new kendo.data.DataSource({
 	    transport: {
 	        read: {
-	            url: "/users",
+	            url: "/posts",
 	            dataType: "json"
-	            
 	        }
 	    }
-	    
-	    
 	});
 	
 	
-	$("#grid").kendoGrid({
-	    dataSource: sharableDataSource,
-	        columns: [
-	        { 
-	        	title: "First Name",
-	            field:"first_name"
-	        },
-	        
-	        {	
-	        	title: "Last Name",
-	            field: "last_name"	            
-	    	}]
-	});
-    
-    
-    
+	$("#posts-grid").kendoGrid({
+		
+		dataSource: PostsData,
+   
+        columns: [
+        {
+            field: "title",
+            title: "Title"
+        },
+        {
+            field: "content",
+            title: "Content",
+            
+    }]
 });
+	
+	
+	
+	
+	
+	window.AppRouter = Backbone.Router.extend({
+		
+		routes: {
+			'users': 'users',
+			'posts': 'posts'
+			
+		},
+		
+		initialize: function() {			
+			this.usersView = new UsersView;
+		},
+		
+		
+		
+		posts: function() {
+			var container = $("#container");
+			container.empty();
+		},
+		
+		
+		
+		users: function() {	
+			
+			var container = $("#container");
+			container.empty();			
+			
+			container.append(this.usersView.render().el);
+			
+			var UsersData = new kendo.data.DataSource({
+				    transport: {
+				        read: {
+				            url: "/users",
+				            dataType: "json"
+				        }
+				    }
+				});				
+				
+				var grid = $("#users-grid").kendoGrid({
+					
+					dataSource: UsersData,
+			   
+			        columns: [
+			        {
+			            field: "first_name",
+			            title: "First Name"
+			        },
+			        {
+			            field: "last_name",
+			            title: "Last Name",
+			            
+			    	}]
+				});
+				
+				container.append(grid);
+				
+		}
+		
+		
+		
+	});
+	
+	
+			
+	
+	
+	
+	window.App = new AppRouter();
+	Backbone.history.start();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    
+    
+// Closing jquery tag    
+});
+
+
+
+
+
+
+
